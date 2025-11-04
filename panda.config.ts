eea3c212981ca5dc3a8292ab9bca2c73edd9c6f6
43mm <18689574+43mm@users.noreply.github.com>
2025-11-04 21:54:27 +1100

    style: define fadein keyframes

--- panda.config.ts
+++ panda.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   jsxFramework: "react",
   theme: {
     extend: {
+      keyframes: {
+        fadein: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
       tokens: {
         fonts: {
           transitional: {
