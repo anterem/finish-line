802229157ce8e2c2f6f30087e71f5bf27fed2ba5
43mm <18689574+43mm@users.noreply.github.com>
2025-11-17 22:07:52 +1100

    chore: remove ssr packages and use spa build

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,18 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 const config = defineConfig({
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
-    tanstackStart(),
+    tanstackStart({
+      spa: {
+        enabled: true,
+      },
+    }),
     viteReact(),
   ],
 });
